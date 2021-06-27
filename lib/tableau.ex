647fbddab0649d0ea6bd09e72367b6cf77bc0a1e
Mitchell Hanberg <mitch@mitchellhanberg.com>
2021-06-27 12:59:55 -0400

    Initial Commit

---
+++ lib/tableau.ex
@@ -0,0 +1,18 @@
+defmodule Tableau do
+  @moduledoc """
+  Documentation for `Tableau`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tableau.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tableau_test.exs
@@ -0,0 +1,8 @@
+defmodule TableauTest do
+  use ExUnit.Case
+  doctest Tableau
+
+  test "greets the world" do
+    assert Tableau.hello() == :world
+  end
+end
