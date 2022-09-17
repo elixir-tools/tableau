e21f778cd1f1b4481f66d7921e7eb97470fb26aa
Mitchell Hanberg <mitch@mitchellhanberg.com>
2022-09-17 17:22:27 -0400

    Add temple back

--- example/mix.exs
+++ example/mix.exs
@@ -32,6 +32,7 @@ defmodule TabDemo.MixProject do
   defp deps do
     [
       {:tableau, path: "../"},
+      {:temple, "~> 0.9"},
       {:benchee, "~> 1.0"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:req, "~> 0.3"},
