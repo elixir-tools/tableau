f9a97a3536ba547012882222c39da80eb907addf
Jeff S <paradox460@gmail.com>
2023-11-14 20:59:14 -0700

    feat: sitemap extension (#45)
    Co-authored-by: Mitchell Hanberg <mitch@mitchellhanberg.com>

---
+++ lib/tableau/extensions/sitemap_extension.ex
@@ -0,0 +1,97 @@
+defmodule Tableau.SitemapExtension.Config do
+  @moduledoc false
+
+  def new(input), do: {:ok, input}
+end
+
+defmodule Tableau.SitemapExtension do
+  @moduledoc """
+  Generate a sitemap.xml for your site, for improved search-engine indexing.
+
+  ## Configuration
+
+  - `:enabled` - boolean - Extension is active or not.
+
+  ### Example
+
+  ```elixir
+  config :tableau, Tableau.SitemapExtension,
+    enabled: true,
+  ```
+
+  ## Custom attributes
+
+  Any page may add attributes to its sitemap `<url>` entry by adding a `sitemap` entry, with a map of elements to add
+
+  ### Frontmatter
+
+  ```yaml
+  title: "Some Page"
+  sitemap:
+    priority: 0.8
+    changefreq: hourly
+  ```
+
+  ### `Tableau.Page`
+
+  ```elixir
+  defmodule MySite.AboutPage do
+    use Tableau.Page,
+      layout: MySite.RootLayout,
+      permalink: "/about",
+      sitemap: %{priority: 0.8, changefreq: "monthly"}
+
+    # ...
+  end
+  ```
+
+  Generated sitemap is stored in the root of your site, at /sitemap.xml.
+
+  For more information about sitemaps, see <https://www.sitemaps.org>.
+  """
+
+  use Tableau.Extension, key: :sitemap, type: :post_write, priority: 300
+
+  def run(%{site: %{config: %{url: root}, pages: pages}} = token) do
+    urls =
+      for page <- pages, uniq: true do
+        loc =
+          [{:loc, nil, URI.merge(root, page.permalink)}]
+          |> prepend_lastmod(page)
+          |> prepend_sitemap_assigns(page)
+
+        {:url, nil, loc}
+      end
+
+    xml =
+      XmlBuilder.document(
+        :urlset,
+        [
+          xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+          "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+          "xsi:schemaLocation":
+            "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+        ],
+        urls
+      )
+
+    File.mkdir_p!("_site")
+    File.write!("_site/sitemap.xml", XmlBuilder.generate_iodata(xml))
+
+    {:ok, token}
+  end
+
+  defp prepend_lastmod(body, %{date: date}) do
+    [{:lastmod, nil, DateTime.to_iso8601(date)} | body]
+  end
+
+  defp prepend_lastmod(body, _), do: body
+
+  defp prepend_sitemap_assigns(body, %{sitemap: sitemap_data}) do
+    for {key, value} <- sitemap_data, reduce: body do
+      acc -> [{key, [], value} | acc]
+    end
+  end
+
+  defp prepend_sitemap_assigns(body, _), do: body
+end
