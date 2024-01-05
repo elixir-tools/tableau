6ac028b143fe67b1c1a5f57866399f2eedf4af97
andyl <andy@r210.com>
2024-01-04 23:10:28 -0800

    Add base_path for Vhost Path Prefix on DevServer

--- lib/mix/tasks/tableau.server.ex
+++ lib/mix/tasks/tableau.server.ex
@@ -11,10 +11,17 @@ defmodule Mix.Tasks.Tableau.Server do
     Application.put_env(:tableau, :server, true)
     Code.put_compiler_option(:ignore_module_conflict, true)
 
-    Logger.debug("server started on http://localhost:4999")
+    Logger.debug("server started on http://localhost:4999#{basepath()}")
 
     Mix.Task.run("app.start", ["--preload-modules"])
 
     Mix.Tasks.Run.run(["--no-halt"])
+  end
+
+  defp basepath do
+    case Application.get_env(:tableau, :config)[:base_path] do
+      "" -> ""
+      path -> "/" <> path
+    end
   end
 end
