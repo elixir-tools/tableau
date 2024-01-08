d79cccbbb0c52b9f7626f756bd4dc4313e9ca13f
andyl <andy@r210.com>
2024-01-07 18:14:17 -0800

    Make moduledoc comment more concise.

--- lib/tableau.ex
+++ lib/tableau.ex
@@ -4,7 +4,7 @@ defmodule Tableau do
 
   * `:include_dir` - string - Directory that is just copied to the output directory. Defaults to `extra`.
   * `:timezone` - string - Timezone to use when parsing date times. Defaults to `Etc/UTC`.
-  * `:base_path - string - base path to use with Github Pages or web-servers that use a location prefix for Vhosts
+  * `:base_path - string - Development server root.  Defaults to '/'.
   * `:url` - string (required) - The URL of your website.
   * `:markdown` - keyword
       * `:mdex` - keyword - Options to pass to `MDEx.to_html/2`
