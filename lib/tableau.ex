588fc7897b67965db42267f1e3248a30a4b766c2
andyl <andy@r210.com>
2024-01-07 18:10:08 -0800

    Remove documentation

--- lib/tableau.ex
+++ lib/tableau.ex
@@ -8,65 +8,6 @@ defmodule Tableau do
   * `:url` - string (required) - The URL of your website.
   * `:markdown` - keyword
       * `:mdex` - keyword - Options to pass to `MDEx.to_html/2`
-
-  ## Working with Github Pages or web-server Vhosts
-
-  Github Pages provides a root url like `https://andyl.github.io/xmeyers`, where
-  the `xmeyers` prefix is a virtual host identifier tied to the repo at
-  `https://github.com/andyl/xmeyers`.
-
-  With Nginx and Apache it is common to use a location prefix for Vhosts.
-  Here is an example NGINX config snippet:
-
-  ```
-  server {
-    listen 80;
-    server_name myhost.com;
-
-    location /site1 {
-        # Configuration for site1 - eg the root directive to a directory
-        # root /var/www/site1;
-    }
-
-    location /site2 {
-        # Configuration for site2
-        # Similar configuration as site1, adjusted for site2 specifics
-    }
-
-    # Other configuration...
-  }
-  ```
-
-  To make Tableau's development server (`mix tableau.server`) also use a Vost
-  prefix, configure your app with the `:base_path` attribute.  With that, your
-  website HREFs will work in both development and production.
-
-  ## Example
-
-  In `config/config.exs`:
-
-  ```elixir
-  config :tableau, :config,
-    url: "http://localhost:4999",
-    base_path: "xmeyers",
-    markdown: [
-      ...
-    ]
-  ```
-
-  In `root_layout.ex`:
-
-  ```elixir
-  def template(assigns) do
-    ~H\"""
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Xmeyers</title>
-        <link rel="icon" href="/xmeyers/static/img/favicon.ico" type="image/x-icon" />
-        <link rel="stylesheet" type="text/css" href="/xmeyers/css/site.css" />
-     ...
-  ```
   """
 
   @doc """
