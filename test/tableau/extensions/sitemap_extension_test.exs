66afcf44532317539687c65bb592d38be66e5c50
Piotr Baj <piotr.baj@petabajt.net>
2025-03-27 09:22:29 +0100

    Code review changes

--- test/tableau/extensions/sitemap_extension_test.exs
+++ test/tableau/extensions/sitemap_extension_test.exs
@@ -25,15 +25,15 @@ defmodule Tableau.SitemapExtensionTest do
     sitemap = File.read!("_site/sitemap.xml")
 
     assert sitemap ===
-             IO.iodata_to_binary([
-               ~s|<?xml version="1.0" encoding="UTF-8"?>|,
-               ~s|<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">|,
-               "<url>",
-               "<lastmod>2018-02-28T00:00:00Z</lastmod>",
-               "<loc>http://example.com/about</loc>",
-               "</url>",
-               "</urlset>"
-             ])
+             """
+             <?xml version="1.0" encoding="UTF-8"?>\
+             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\
+             <url>\
+             <lastmod>2018-02-28T00:00:00Z</lastmod>\
+             <loc>http://example.com/about</loc>\
+             </url>\
+             </urlset>\
+             """
   end
 
   @tag :tmp_dir
@@ -62,15 +62,15 @@ defmodule Tableau.SitemapExtensionTest do
     sitemap = File.read!("#{tmp_dir}/sitemap.xml")
 
     assert sitemap ===
-             IO.iodata_to_binary([
-               ~s|<?xml version="1.0" encoding="UTF-8"?>|,
-               ~s|<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">|,
-               "<url>",
-               "<priority>0.5</priority>",
-               "<changefreq>monthly</changefreq>",
-               "<loc>http://example.com/about</loc>",
-               "</url>",
-               "</urlset>"
-             ])
+             """
+             <?xml version="1.0" encoding="UTF-8"?>\
+             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\
+             <url>\
+             <priority>0.5</priority>\
+             <changefreq>monthly</changefreq>\
+             <loc>http://example.com/about</loc>\
+             </url>\
+             </urlset>\
+             """
   end
 end
