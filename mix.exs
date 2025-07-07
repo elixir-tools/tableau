0eb74c0b55e495845a774e7ff46962a782b9cfd4
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-07-06 22:55:25 -0400

    feat!: bump mdex to 0.7
    The configuration options for MDEx have changed, as well as the syntax
    themes available.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Tableau.MixProject do
       {:date_time_parser, "~> 1.2"},
       {:html_entities, "~> 0.5.2"},
       {:libgraph, "~> 0.16.0"},
-      {:mdex, "~> 0.4.0"},
+      {:mdex, "~> 0.7.0"},
       {:schematic, "~> 0.5.1"},
       {:slugify, "~> 1.3"},
       {:tz, "~> 0.28.1"},
